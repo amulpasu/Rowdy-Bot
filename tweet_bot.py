1280d982d6d9ed43406c160c9c9281148823a5a8
amulpasu <amulpasu@gmail.com>
2019-03-16 11:16:50 +0530

    commit_message

--- tweet_bot.py
+++ tweet_bot.py
@@ -2,10 +2,10 @@ import tweepy, time
 #from credentials import *
 
 from os import environ
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+access_token = environ['ACCESS_KEY']
+access_token_secret = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
